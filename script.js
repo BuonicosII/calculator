bc8e62e9ba1ce2349e6f953a4a3ed099a9af5072
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-02-05 13:18:56 +0100

    Create function operate
    Created function operate in order to have the math function run through
    the UI, when it will be implemented in the html.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //the functions for the four basic math operations
-
 //add
 
 function add (num1, num2) {
@@ -24,3 +23,21 @@ function divide (num1, num2) {
     return num1 / num2;
 };
 
+//function operate which calls one math function on two operands
+
+function operate(operand1, operator, operand2) {
+    switch(operator) {
+        case "+":
+            return add(operand1, operand2);
+            break;
+        case "-":
+            return subtract(operand1,operand2);
+            break;
+        case "x":
+            return multiply(operand1,operand2);
+            break;
+        case ":":
+            return divide(operand1,operand2);
+            break;
+    }
+}
