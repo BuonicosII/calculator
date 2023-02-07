4d9dbc40f9aedb9cba830f15b6d6410da2ab2c76
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-02-07 23:32:46 +0100

    Add key function to calculator
    - Now it is possibile to run the calculator completely through the
      keyboard
    - Fixed a minor bug that caused the calculator not to work properly after
      more than one operation had been run and C had been pressed

--- script.js
+++ script.js
@@ -39,10 +39,10 @@ function operate(operand1, operator, operand2) {
         case "-":
             return subtract(operand1,operand2);
             break;
-        case "x":
+        case "*":
             return multiply(operand1,operand2);
             break;
-        case ":":
+        case "/":
             return divide(operand1,operand2);
             break;
         case "xy":
@@ -103,6 +103,39 @@ buttonsDigits.forEach((button) => {
 }
 );
 
+//Number keys
+//function to populate the display with the selected numbers keys
+document.addEventListener('keydown', function(e) {
+    if (e.key === '1' || e.key === '2' || e.key === '3' || e.key === '4' || e.key === '5' ||
+        e.key === '6' || e.key === '7' || e.key === '8' || e.key === '9' || e.key === '0' || e.key === '.') {
+            if (resultChecker === 1 && displayText.textContent !== '') {
+
+                resultChecker = 0;
+                upperDisplay.textContent = '';
+                displayText.textContent = e.key;
+    
+            } else if (displayText.textContent === '0' && e.key === '.' 
+            || displayText.textContent === '' && e.key === '.') {
+    
+                displayText.textContent = '0' + e.key;
+        
+            } else if (displayText.textContent === '0') {
+    
+                displayText.textContent = e.key;
+                
+            } else if (e.key === '.' && displayText.textContent.includes(".")) {
+                
+                displayText.textContent = displayText.textContent;
+                
+            } else {
+    
+                displayText.textContent += e.key;
+    
+            }
+    }
+});
+
+
 //Operator buttons
 //function to store the display number in a variable and generate the operator variable
 //when the corrisponding button is clicked
@@ -121,7 +154,8 @@ buttonsOperator.forEach((button) => {
             operator = e.target.textContent;
             upperDisplay.textContent += ` ${operator}`;
             displayText.textContent = '';
-            operand2 = '';;
+            operand2 = '';
+            resultChecker = 2;
         } else {
         operand1 = displayText.textContent; 
         operator = e.target.textContent;
@@ -132,13 +166,39 @@ buttonsOperator.forEach((button) => {
 }
 );
 
+//Operator key
+//function to store the display number in a variable and generate the operator variable
+//when the corrisponding key is pressed
+document.addEventListener('keydown', function(e) {
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+    if (displayText.textContent === '' && operator !== '') {
+        operator = e.key;
+        upperDisplay.textContent = `${operand1} ${operator}`;
+        displayText.textContent = '';
+    } else if (displayText.textContent !== '' && operator !== '') {
+        operand2 = displayText.textContent;
+        upperDisplay.textContent = `${operate(operand1, operator, operand2)}`;
+        operand1 = operate(operand1, operator, operand2);
+        operator = e.key;
+        upperDisplay.textContent += ` ${operator}`;
+        displayText.textContent = '';
+        operand2 = '';;
+    } else {
+    operand1 = displayText.textContent; 
+    operator = e.key;
+    upperDisplay.textContent = `${operand1} ${operator}`;
+    displayText.textContent = '';
+    }
+}
+});
+
 //Equals button
 //function to store the display number in another variable and run the operate function
 //when equal is clicked upon
 
 let equalOperator = document.querySelector('#equals');
 equalOperator.addEventListener('click', function() {
-    if (displayText.textContent === '0' && operator === ':' || displayText.textContent === '' && operator === ':') {
+    if (displayText.textContent === '0' && operator === '/' || displayText.textContent === '' && operator === '/') {
         alert('Nice try, don\'t break my calculator, thx ;-)');
         displayText.textContent = '';
     } else {
@@ -150,6 +210,26 @@ equalOperator.addEventListener('click', function() {
     operator = '';
     resultChecker = 1;
 }
+});
+
+//Equals key function
+//function to store the display number in another variable and run the operate function
+//when equal key is pressed on the keyboard 
+document.addEventListener('keydown', function(e) {
+    if (e.key === '=' || e.key === 'Enter') {
+        if (displayText.textContent === '0' && operator === '/' || displayText.textContent === '' && operator === '/') {
+            alert('Nice try, don\'t break my calculator, thx ;-)');
+            displayText.textContent = '';
+        } else {
+        operand2 = displayText.textContent;
+        upperDisplay.textContent = `${operand1} ${operator} ${operand2}`;
+        displayText.textContent = `${operate(operand1, operator, operand2)}`;
+        operand1 = '';
+        operand2 = '';
+        operator = '';
+        resultChecker = 1;
+        }
+    }
 });
 
 //AC button
@@ -165,6 +245,7 @@ acButton.addEventListener('click', function() {
     upperDisplay.textContent = '';
 });
 
+//AC key
 //function to reset the calculator and all of its values when the Del Key is pressed
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Delete') {
@@ -183,7 +264,7 @@ document.addEventListener('keydown', function(e) {
 
 let cButton = document.querySelector('#c');
 cButton.addEventListener('click', function () {
-    if (displayText.textContent === '') {
+    if (displayText.textContent === '' && resultChecker !== 2) {
         operator = '';
         displayText.textContent = operand1;
         upperDisplay.textContent = ''; 
@@ -191,6 +272,12 @@ cButton.addEventListener('click', function () {
         displayText.textContent = '0';
         upperDisplay.textContent = '';
         resultChecker = 0;
+    } else if (resultChecker === 2) {
+        displayText.textContent = operand1;
+        upperDisplay.textContent = '';
+        operator = '';
+        operand1 = '';
+        resultChecker = 1;
     } else if (resultChecker === 0 && displayText.textContent === '0'
                 || resultChecker === 0 && displayText.textContent.length === 1 && operator === '') {
         displayText.textContent = '0';
@@ -199,12 +286,13 @@ cButton.addEventListener('click', function () {
     }
 });
 
+//C key
 //function to remove last added character to the number on the display or void the 
 //selected operator when backspace is pressed
 
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Backspace') {
-        if (displayText.textContent === '') {
+        if (displayText.textContent === '' && resultChecker !== 2) {
             operator = '';
             displayText.textContent = operand1;
             upperDisplay.textContent = ''; 
@@ -212,6 +300,12 @@ document.addEventListener('keydown', function(e) {
             displayText.textContent = '0';
             upperDisplay.textContent = '';
             resultChecker = 0;
+        } else if (resultChecker === 2) {
+            displayText.textContent = operand1;
+            upperDisplay.textContent = '';
+            operator = '';
+            operand1 = '';
+            resultChecker = 1;
         } else if (resultChecker === 0 && displayText.textContent === '0'
                     || resultChecker === 0 && displayText.textContent.length === 1 && operator === '') {
             displayText.textContent = '0';
